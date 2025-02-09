c835e8389c26b0545cd4ea9e315f23d2132c7af5
sherryyu <bambooyu715@gmail.com>
2025-02-09 15:20:14 -0500

    finished cancel likes serializer, api, and test

--- likes/api/tests.py
+++ likes/api/tests.py
@@ -1,6 +1,7 @@
 from testing.testcase import TestCase 
 
 LIKE_BASE_URL = "/api/likes/"
+LIKE_CANCEL_URL = "/api/likes/cancel/"
 
 class LikeApiTests(TestCase):
 
@@ -61,3 +62,45 @@ class LikeApiTests(TestCase):
         self.assertEqual(comment.like_set.count(), 1)
         self.panda_client.post(LIKE_BASE_URL, data)
         self.assertEqual(comment.like_set.count(), 2)
+
+    def test_cancel(self):
+        tweet = self.create_tweet(self.sherry)
+        comment = self.create_comment(self.panda, tweet)
+        like_comment_data = {"content_type": "comment", "object_id": comment.id}
+        like_tweet_data = {"content_type": "tweet", "object_id": tweet.id}
+
+        self.panda_client.post(LIKE_BASE_URL, like_tweet_data)
+        self.assertEqual(tweet.like_set.count(), 1)
+        self.sherry_client.post(LIKE_BASE_URL, like_comment_data)
+        self.assertEqual(comment.like_set.count(), 1)
+
+        # login required 
+        response = self.anonymous_client.post(LIKE_BASE_URL, like_comment_data)
+        self.assertEqual(response.status_code, 403)
+
+        # get is not allowed 
+        response = self.panda_client.get(LIKE_BASE_URL, like_tweet_data)
+        self.assertEqual(response.status_code, 405)
+
+        # wrong content type 
+        response = self.panda_client.post(LIKE_BASE_URL, {
+            "content_type": "twet",
+            "object_id": tweet.id,
+        })
+        self.assertEqual(response.status_code, 400)
+
+        # wrong object_id
+        response = self.panda_client.post(LIKE_BASE_URL, {
+            "content_type": "tweet",
+            "object_id": -1,
+        })
+        self.assertEqual(response.status_code, 400)
+
+        # successful cancel 
+        response = self.panda_client.post(LIKE_CANCEL_URL, like_tweet_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(tweet.like_set.count(), 0)
+
+        response = self.sherry_client.post(LIKE_CANCEL_URL, like_comment_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(comment.like_set.count(), 0)
