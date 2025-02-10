4a5130f84087a7507a37305b9e23869cbc667b4f
sherryyu <bambooyu715@gmail.com>
2025-02-10 16:35:44 -0500

    set up redis and tested the set up

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,7 +1,9 @@
 from testing.testcase import TestCase
-from .models import Tweet 
 from datetime import timedelta 
 from utils.time_helpers import utc_now 
+from utils.redis_client import RedisClient 
+from utils.redis_serializers import DjangoModelSerializer 
+
 
 
 class TweetTests(TestCase):
@@ -25,5 +27,17 @@ class TweetTests(TestCase):
         self.panda = self.create_user("panda")
         self.create_like(self.panda, self.tweet)
         self.assertEqual(self.tweet.like_set.count(), 2)
+
+    def test_cache_tweet_in_redis(self):
+        tweet = self.create_tweet(self.sherry)
+        conn = RedisClient.get_connection()
+        serialized_tweet = DjangoModelSerializer.serialize(tweet)
+        conn.set("tweet:{}".format(tweet.id), serialized_tweet)
+        data = conn.get("tweet:not_exist")
+        self.assertEqual(data, None) 
+
+        data = conn.get("tweet:{}".format(tweet.id))
+        cached_tweet = DjangoModelSerializer.deserialize(data)
+        self.assertEqual(tweet, cached_tweet)
 
 
