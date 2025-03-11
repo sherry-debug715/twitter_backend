a3cf99f7690e8e32c2caa6835ab3d1e8b77db959
sherryyu <bambooyu715@gmail.com>
2025-03-11 15:30:21 -0400

    completed tweet photo model

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,9 +1,11 @@
-from testing.testcase import TestCase
+from utils.tests import TestCase
 from datetime import timedelta 
+from django.core.files.uploadedfile import SimpleUploadedFile
 from utils.time_helpers import utc_now 
 from utils.redis_client import RedisClient 
 from utils.redis_serializers import DjangoModelSerializer 
-
+from tweets.constants import TweetPhotoStatus
+from tweets.models import TweetPhoto
 
 
 class TweetTests(TestCase):
@@ -28,16 +30,30 @@ class TweetTests(TestCase):
         self.create_like(self.panda, self.tweet)
         self.assertEqual(self.tweet.like_set.count(), 2)
 
-    def test_cache_tweet_in_redis(self):
-        tweet = self.create_tweet(self.sherry)
-        conn = RedisClient.get_connection()
-        serialized_tweet = DjangoModelSerializer.serialize(tweet)
-        conn.set("tweet:{}".format(tweet.id), serialized_tweet)
-        data = conn.get("tweet:not_exist")
-        self.assertEqual(data, None) 
+    # def test_cache_tweet_in_redis(self):
+    #     tweet = self.create_tweet(self.sherry)
+    #     conn = RedisClient.get_connection()
+    #     serialized_tweet = DjangoModelSerializer.serialize(tweet)
+    #     conn.set("tweet:{}".format(tweet.id), serialized_tweet)
+    #     data = conn.get("tweet:not_exist")
+    #     self.assertEqual(data, None) 
 
-        data = conn.get("tweet:{}".format(tweet.id))
-        cached_tweet = DjangoModelSerializer.deserialize(data)
-        self.assertEqual(tweet, cached_tweet)
+    #     data = conn.get("tweet:{}".format(tweet.id))
+    #     cached_tweet = DjangoModelSerializer.deserialize(data)
+    #     self.assertEqual(tweet, cached_tweet)
+
+    def test_create_photo(self):
+        photo = TweetPhoto.objects.create(
+            user=self.sherry,
+            tweet=self.tweet,
+            file=SimpleUploadedFile(
+                name="test_image.jpg",
+                content=str.encode("a fake image"),
+                content_type="image/jpeg"
+            ),
+        )
+        self.assertEqual(photo.status, TweetPhotoStatus.PENDING)
+        self.assertEqual(photo.user, self.sherry) 
+        self.assertEqual(self.tweet.tweetphoto_set.count(), 1)
 
 
